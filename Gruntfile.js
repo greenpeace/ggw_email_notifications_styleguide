c01423b0d563f73537b70f67c5649e82301b7fd4
remy <remy@stripthis.com>
2016-06-01 12:17:37 +0530

    CSS build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,28 @@ module.exports = function(grunt) {
       },
       tmp: {
         src: ['tmp/**']
+      },
+      css: {
+        src: ['src/shared/partials/css/*','!src/shared/partials/css/outlook_css.hbs']
+      }
+    },
+    less: {
+      files: {
+        expand: true,
+        flatten: true,
+        cwd: "less/",
+        src: "*.less",
+        dest: "tmp/css",
+        ext: ".css"
+      }
+    },
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: 'tmp/css/',
+        src: ['*.css'],
+        dest: 'src/shared/partials/css/',
+        ext: '.hbs'
       }
     },
     inlinecss: {
@@ -50,13 +72,16 @@ module.exports = function(grunt) {
 
   // Load the plugins
   grunt.loadNpmTasks('grunt-inline-css');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-compile-handlebars');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-less');
 
   // Default task(s).
-  grunt.registerTask('default', ['clean:build','compile-handlebars','inlinecss','clean:tmp']);
+  grunt.registerTask('default', ['clean:css','less','cssmin','clean:build','compile-handlebars','inlinecss','clean:tmp']);
   grunt.registerTask('compile', ['compile-handlebars']);
   grunt.registerTask('cleanup', ['clean:build','clean:tmp']);
+  grunt.registerTask('css', ['clean:css','less','cssmin','clean:tmp']);
 
 };
